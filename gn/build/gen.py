b0e6bada9d4da53204dffa7d9343f925c7d5603e
Samuli Piippo <samuli.piippo@qt.io>
2017-03-15 13:53:28 +0200

    chromium: Force host toolchain configuration
    Force gcc/g++ to be used for parts using host toolchain, since
    the option(host_build) does not work in yocto builds.
    
    Upstream-Status: Inappropriate [OE specific]
    Signed-off-by: Samuli Piippo <samuli.piippo@qt.io>
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- gn/build/gen.py
+++ gn/build/gen.py
@@ -310,10 +310,10 @@ def WriteGNNinja(path, platform, host, options, linux_sysroot):
     ld = os.environ.get('LD', 'g++')
     ar = os.environ.get('AR', 'ar -X64')
   else:
-    cc = os.environ.get('CC', 'cc')
-    cxx = os.environ.get('CXX', 'c++')
+    cc = os.environ.get('CC_host', 'gcc')
+    cxx = os.environ.get('CXX_host', 'g++')
     ld = cxx
-    ar = os.environ.get('AR', 'ar')
+    ar = os.environ.get('AR_host', 'ar')
 
   # QTBUG-64759
   # cflags = os.environ.get('CFLAGS', '').split()
