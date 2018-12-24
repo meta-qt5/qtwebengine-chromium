e1ee87667769a11f00b3c085cac09497702f3651
Khem Raj <raj.khem@gmail.com>
2018-12-23 16:58:04 -0800

    chromium: stack pointer clobber
    Do not add stack pointer to clobber list
    
    it was being ignored until gcc 9.0 became capable
    of flagging this silent ignoring via [1]
    
    [1] https://gcc.gnu.org/bugzilla/show_bug.cgi?id=52813<Paste>
    
    Upstream-Status: Submitted [https://chromium-review.googlesource.com/c/linux-syscall-support/+/1390160]
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- chromium/third_party/lss/linux_syscall_support.h
+++ chromium/third_party/lss/linux_syscall_support.h
@@ -2012,7 +2012,7 @@ struct kernel_statfs {
                              : "i" (__NR_##name), "ri" ((long)(arg1)),        \
                                "c" ((long)(arg2)), "d" ((long)(arg3)),        \
                                "S" ((long)(arg4)), "D" ((long)(arg5))         \
-                             : "esp", "memory");                              \
+                             : "memory");                                     \
         LSS_RETURN(type,__res);                                               \
       }
     #undef  _syscall6
@@ -2034,7 +2034,7 @@ struct kernel_statfs {
                              : "i" (__NR_##name),  "0" ((long)(&__s)),        \
                                "c" ((long)(arg2)), "d" ((long)(arg3)),        \
                                "S" ((long)(arg4)), "D" ((long)(arg5))         \
-                             : "esp", "memory");                              \
+                             : "memory");                                     \
         LSS_RETURN(type,__res);                                               \
       }
     LSS_INLINE int LSS_NAME(clone)(int (*fn)(void *), void *child_stack,
@@ -2120,7 +2120,7 @@ struct kernel_statfs {
                            : "0"(-EINVAL), "i"(__NR_clone),
                              "m"(fn), "m"(child_stack), "m"(flags), "m"(arg),
                              "m"(parent_tidptr), "m"(newtls), "m"(child_tidptr)
-                           : "esp", "memory", "ecx", "edx", "esi", "edi");
+                           : "memory", "ecx", "edx", "esi", "edi");
       LSS_RETURN(int, __res);
     }
 
